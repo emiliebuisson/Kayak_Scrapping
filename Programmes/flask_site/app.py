7e812131d16e414875619a1ff0d7af8a973a538b
Emilie Buisson <emilie.buisson@edu.esiee.fr>
2023-02-25 23:49:55 +0100

    Statistiques fonctionnel

--- Programmes/flask_site/app.py
+++ Programmes/flask_site/app.py
@@ -2,14 +2,11 @@ from flask import Flask, request, render_template, redirect, url_for
 from traitement_donnees import * 
 from data import insert_json_files
 
-# Flask constructor
 app = Flask(__name__)  
 
-# load
+# chargement de la base de données
 insert_json_files()
 
-# A decorator used to tell the application
-# which URL is associated function
 
 @app.route('/', methods =["GET"])
 def accueil():
@@ -35,29 +32,31 @@ def gfg():
 def destinations():
     villes = ["", "tunis", "montreal", "bangkok", "newyork", "marrakech", "venice", "madrid", "berlin", "london", "oslo"]
     documents = []
-    final = []
-    name_city = []
     if request.method == "POST":
         ville = request.form.get("ville")
         if ville: 
             documents = get_documents(ville)
-            final, name_city = get_random_documents()
             return redirect(url_for("ville", ville=ville, documents=documents))
-    return render_template("destinations.html", villes=villes, documents=documents, final = final, name_city = name_city)
+    return render_template("destinations.html", villes=villes, documents=documents)
 
-# @app.route('/destinations', methods=["GET", "POST"])
-# def destinations():
-#     villes = ["", "tunis", "montreal", "bangkok", "newyork", "marrakech", "venice", "madrid", "berlin", "london", "oslo"]
-#     if request.method == "POST":
-#         ville = request.form.get("ville")
-#         if ville:
-#             return redirect(url_for("ville", ville=ville))
-#     return render_template("destinations.html", villes=villes)
 
-@app.route('/destinations/<ville>')
+@app.route('/destinations/<ville>', methods=["GET", "POST"])
 def ville(ville):
+    villes = ["", "tunis", "montreal", "bangkok", "newyork", "marrakech", "venice", "madrid", "berlin", "london", "oslo"]
+    documents = []
     documents = get_documents(ville)
-    return render_template("ville.html", ville=ville, documents=documents)
+    if request.method == "POST":
+        other_ville = request.form.get("other_ville")
+        if other_ville:
+            documents = get_documents(other_ville)
+            return redirect(url_for("other_ville", ville=ville, documents=documents))
+    return render_template("ville.html", villes=villes, documents=documents, ville= ville)
+
+
+# @app.route('/destinations/<ville>')
+# def ville(ville):
+#     documents = get_documents(ville)
+#     return render_template("ville.html", ville=ville, documents=documents)
 
 
 @app.route('/statistiques', methods=["GET", "POST"])
@@ -66,17 +65,17 @@ def statistiques():
     if request.method == "POST":
         ville = request.form.get("ville")
         if ville:
-            days, prices, price_stats, companies_data, companies = get_stats(ville)
-            graphJSON1 = plot_prices(days, prices)
-            graphJSON2 = plot_company_prices(companies_data)
-            graphJSON3 = pie_chart(companies)
-            return redirect(url_for("statville", ville=ville, graphJSON1 = graphJSON1, graphJSON2 = graphJSON2, graphJSON3 = graphJSON3))
+            
+            return redirect(url_for("statville", ville=ville))
     return render_template("statistiques.html", villes=villes)
 
 @app.route('/statistiques/<ville>')
 def statville(ville):
-    
-    return render_template("statville.html", ville=ville)
+    days, prices, price_stats, companies_data, companies = get_stats(ville)
+    graphJSON1 = plot_prices(days, prices)
+    graphJSON2 = plot_company_prices(companies_data)
+    graphJSON3 = pie_chart(companies)
+    return render_template("statville.html", ville=ville, graphJSON1 = graphJSON1, graphJSON2 = graphJSON2, graphJSON3 = graphJSON3)
 
 if __name__=='__main__':
    app.run(debug=True,host='0.0.0.0', port="5066")
